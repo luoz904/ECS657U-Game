79aa1b3390f4ff766a5d1c1eb6a95cf190057c9e
Daniel Torquato <daniel.torquato@icomp.ufam.edu.br>
2024-12-21 18:22:50 +0000

    dev: add mage and target mechanism

---
+++ Assets/mageMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class mageMovement : MonoBehaviour
+{
+
+    public Transform target = null;
+
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+
+    public float turnSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget() {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float nearestEnemeyDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies) {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < nearestEnemeyDistance) {
+                nearestEnemeyDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && nearestEnemeyDistance <= range) {
+            target = nearestEnemy.transform;
+        } else {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            return;
+        
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, turnSpeed * Time.deltaTime).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
