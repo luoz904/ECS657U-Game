324dde8165cc76b649639816c1426de7fb38952b
Luiz Lopes <luizcarlosfilho@hotmail.co.uk>
2024-12-21 18:22:50 +0000

    dev: add mage and target mechanism

--- Assets/enemySpawner.cs
+++ Assets/enemySpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemySpawner : MonoBehaviour
 {
@@ -12,31 +13,37 @@ public class enemySpawner : MonoBehaviour
     public float timeBetweenWaves = 5f;
 
     private float countDown = 2f;
-    
+
+    public Text waveCountDownText;
+
     private int waveNumber = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if (countDown <= 0f) {
-            StartCoroutine(SpawnWave());
+        if (countDown <= 0f)
+        {
             countDown = timeBetweenWaves;
+            StartCoroutine(SpawnWave());
         }
 
         countDown -= Time.deltaTime;
+        waveCountDownText.text = Mathf.Round(countDown).ToString();
     }
 
-    IEnumerator SpawnWave() {
+    IEnumerator SpawnWave()
+    {
         waveNumber++;
-        Debug.Log("Wave Incomming!");
+
         for (int i = 0; i < waveNumber; i++)
         {
             SpawnEnemy();
-            yield return new  WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.5f);
         }
     }
 
-    void SpawnEnemy() {
+    void SpawnEnemy()
+    {
         Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 }
