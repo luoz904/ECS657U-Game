61849ee3565590470c18f498dc852b9d4b2f6a34
Luiz Lopes <luizcarlosfilho@hotmail.co.uk>
2024-12-22 16:13:25 +0000

    dev: add camera control

--- Assets/cameraControl.cs
+++ Assets/cameraControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,37 +6,53 @@ using UnityEngine;
 public class cameraControl : MonoBehaviour
 {
 
-    public float panSpeed = 30f;
+  private bool doMovement = true;
 
-    public float borderPad = 10f;
+  public float panSpeed = 30f;
 
-    // Start is called before the first frame update
-    void Start()
+  public float borderPad = 10f;
+
+  public float scrollSpeed = 5f;
+
+  public float minY = 10f;
+  public float maxY = 80f;
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Escape))
+      doMovement = !doMovement;
+
+    if (!doMovement)
+      return;
+
+    if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - borderPad)
     {
-
+      transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
     }
 
-    // Update is called once per frame
-    void Update()
+    if (Input.GetKey("a") || Input.mousePosition.x <= borderPad)
     {
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - borderPad)
-        {
-            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
-        }
-
-          if (Input.GetKey("a") || Input.mousePosition.x <= borderPad)
-        {
-            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
-        }
-
-          if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - borderPad)
-        {
-            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
-        }
-
-          if (Input.GetKey("s") || Input.mousePosition.y <= borderPad)
-        {
-            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
-        }
+      transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
     }
+
+    if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - borderPad)
+    {
+      transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+    }
+
+    if (Input.GetKey("s") || Input.mousePosition.y <= borderPad)
+    {
+      transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+    }
+
+    float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+    Vector3 pos = transform.position;
+
+    pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+    pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+    transform.position = pos;
+  }
 }
