6f8d0d02301f3d5eaa051db30a175ba1dc55e88a
Daniel Torquato <daniel.torquato@icomp.ufam.edu.br>
2024-12-27 19:26:15 +0000

    dev: add new mage tower
    (cherry picked from commit 4648ac49f449b9a65c81af4f3027195faa26e239)

--- Assets/Scripts/bulletMovement.cs
+++ Assets/Scripts/bulletMovement.cs
@@ -18,11 +18,6 @@ public class bulletMovement : MonoBehaviour
         target = _target;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
